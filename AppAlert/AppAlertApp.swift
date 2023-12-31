008336a4f24a7e5fd20ee3c52f4512768970c107
Tim Mitra <tim@it-guy.com>
2023-12-31 15:10:23 -0500

    Initial Commit

---
+++ AppAlert/AppAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAlertApp.swift
+//  AppAlert
+//
+//  Created by Tim Mitra on 2023-12-31.
+//
+
+import SwiftUI
+
+@main
+struct AppAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppAlert
+//
+//  Created by Tim Mitra on 2023-12-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
