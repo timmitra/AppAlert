e6397856ed7b844656c4a1ad2be3046dca236a07
Tim Mitra <tim@it-guy.com>
2023-12-31 15:46:06 -0500

    add alert to the view

--- AppAlert/ContentView.swift
+++ AppAlert/ContentView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var alertService = AlertService(
+    "https://timmitra.github.io/AppAlert/messages.json"
+  )
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+      .imageScale(.large)
+      .foregroundStyle(.tint)
+      Text("Hello, world!")
+      .alert(
+        alertService.message.title,
+        isPresented: $alertService.showMessage
+      ) {
+        Button(alertService.message.confirmLabel) {
         }
-        .padding()
+      } message: {
+        Text(alertService.message.text)
+        }
     }
+    .padding()
+    // use task not onAppear since it's async
+    .task {
+      await alertService.showAlertIfNecessary()
+    }
+  }
 }
 
 #Preview {
